4be1fa175d196c555d82b51e7b13e56a93a15c3a
Scepti <40700483+Scepti@users.noreply.github.com>
2018-08-03 19:42:38 -0500

    Update scepti-bot.py

--- scepti-bot.py
+++ scepti-bot.py
@@ -3,7 +3,9 @@ import discord
 from discord.ext.commands import Bot
 from discord.ext import commands
 import asyncio
-
+Client = discord.Client()
+client = commands.Bot(command_prefix = "?")
+@client.event
 async def on_ready():
 	print("Ready")
 
